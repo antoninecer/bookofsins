347caf50702a4614cf8362f68465d78474c799bf
Antonin75 <antoninecer@gmail.com>
2020-01-01 19:43:48 +0100

    know issue
    Passwords in db are visible - must be fixed

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'registr');
+   define('DB_PASSWORD', 'RegHes123');
+   define('DB_DATABASE', 'registr');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
---
+++ welcome.php
@@ -0,0 +1,23 @@
+<?php
+   include('session.php');
+?>
+<html">
+   
+   <head>
+      <title>Registr </title>
+	  <meta charset="UTF-8">
+   </head>
+   
+   <body>
+    <?php include('menu.php'); ?>
+      <br>
+	  <br>
+	  Sem napsat o co jde a jak se s tim pracuje
+	  <br>
+	  při hledání % najde vše
+	  
+	  <hr>
+	  
+   </body>
+   
+</html>
